be3b6a6b36c11c526d7154eea63cf6e222e61d73
vwvladlen <169064793+vwvladlen@users.noreply.github.com>
2024-05-11 00:15:53 +0300

    Create range.js

---
+++ range.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const range = null;
+  const range = (a, b) => {
+    const arrRange = [];
+    for (let i = a; i <= b; i++){
+      arrRange.push(i);
+    }
+    return arrRange;
+  };
+
+module.exports = { range };
