5223aff88314f559f1ff4f76a7b159e70f3a0b1e
vwvladlen <169064793+vwvladlen@users.noreply.github.com>
2024-05-10 23:35:15 +0300

    Update scalar.js

--- scalar.js
+++ scalar.js
@@ -1,10 +1,1 @@
-let a = 5;
-let b = a;
-a--;
-b++;
-console.dir({ a, b });
-
-const objectA = { field: 5 };
-const objectB = objectA;
-objectA.field = 6;
-console.dir({ objectA, objectB });
+const inc = x => ++x;
